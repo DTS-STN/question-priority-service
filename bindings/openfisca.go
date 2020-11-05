4d92a3c9e701bc5be87cc5536c88fe23a31cc233
will0684 <jordan.walcaraz@gmail.com>
2020-11-05 10:21:35 -0500

    Request and response structure modified

--- bindings/openfisca.go
+++ bindings/openfisca.go
@@ -1,5 +1,15 @@
 package bindings
 
+// NextQuestionRequest is the request sent by the client that contains the information
+// required for the QPS to determine the next question.
 type NextQuestionRequest struct {
-	Key string
+	// Date period for request
+	RequestDate string `json:"request_date"`
+	// Array of life journeys, which represent a subset of benefits
+	LifeJourneys []string `json:"life_journeys"`
+	// Array of specific benefits to get the questions for
+	BenefitList []string `json:"benefit_list"`
+	// List of answered priority questions with their answers and the next priority 
+	// question with a value of null
+	QuestionList []interface{} `json:"client_response"`
 }
--- renderings/openfisca.go
+++ renderings/openfisca.go
@@ -1,7 +1,22 @@
 package renderings
 
+// NextQuestionResponse is the response returned to the client that contains
+// the same information received in the request, with the added property of
+// BenefitElegibility, which contains a list of benefits to which the client
+// has been deemed eligible or inelegible.
 type NextQuestionResponse struct {
 	Success bool   `json:"success"`
 	Message string `json:"message"`
-	Key string `json:"key"`
+	// Date period for request
+	RequestDate string `json:"request_date"`
+	// Array of life journeys, which represent a subset of benefits
+	LifeJourneys []string `json:"life_journeys"`
+	// Array of specific benefits to get the questions for
+	BenefitList []string `json:"benefit_list"`
+	// List of answered priority questions with their answers and the next priority 
+	// question with a value of null
+	QuestionList []interface{} `json:"client_response"`
+	// List of eligible and non-eligible benefits, populated as responses to
+	// prioritized questions are recieved
+	BenefitElegibility []interface{} `json:"benefit_elegibility"`
 }
